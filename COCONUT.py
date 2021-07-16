737071a56f9e2936bd46f696ac04fa16538334ff
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2021-07-16 07:42:39 +0530

    Add files via upload

---
+++ COCONUT.py
@@ -0,0 +1,7 @@
+T = int(input())
+for i in range (T):
+    xa , xb , Xa , Xb=map(int,input().split())
+    coconutsA=Xa/xa
+    coconutsB=Xb/xb
+    print(int(coconutsA+coconutsB))
+# cook your dish here
