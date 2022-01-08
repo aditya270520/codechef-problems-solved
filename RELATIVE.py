374a087e65482d14c1566cbd28f659bce67f4a36
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-09 00:10:39 +0530

    Add files via upload

---
+++ RELATIVE.py
@@ -0,0 +1,12 @@
+# cook your dish here
+try:
+    for i in range(int(input())):
+        g,c = map(int, input().split( ))
+        v = c*c 
+        height = int(v/(2*g))
+        print(height)
+except:
+     pass3
+# 7 1400
+# 5 1000
+# 10 1000
