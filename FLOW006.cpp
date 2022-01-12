97d1951572e70a4206d99a0c6f21fca18560d7ce
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-12 11:55:33 +0530

    Add files via upload

---
+++ FLOW006.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T, n;
+    cin>>T;
+    while(T--){
+        cin>>n;
+        int sum=0;
+        while(n>0){
+           sum+= n%10;
+           n= n/10;
+        }
+        cout<<sum<<endl;
+    }
+	return 0;
+}
+ 
