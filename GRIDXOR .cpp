deec65ad44c36b099662041c907e1b25ddf40df1
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-13 10:41:05 +0530

    Add files via upload

---
+++ GRIDXOR .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // your code goes here
+	int TESTCASE = 1;
+	cin>>TESTCASE;
+	while(TESTCASE--){
+    int i,j,N;
+    cin>>N;//Takes input from the user for size
+    for(i=1; i<=N; i++){
+        for(j=1; j<=N; j++){
+        cout<<"1 ";
+    }
+    
+   cout<<"\n";
+    }
+    }return 0;
+}
