a2c6e95237e3e3ddc8241fa14137efe65d86b896
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-20 09:34:32 +0530

    Add files via upload

---
+++ EQCARDGAME .cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int TESTCASE = 1;
+	cin>>TESTCASE;
+	while(TESTCASE--){
+	    int K ,A;
+	    cin>>K;
+	    A =52%K;
+	    cout << A << endl;
+	}
+	return 0;
+}
