eea475b439b32de7f0bb85c4af697acc91ef413b
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-20 09:49:23 +0530

    Add files via upload

---
+++ BUYPLSE.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b, x, y ,price;
+	cin>>a>>b>>x>>y ;
+	price = a*x+b*y ;
+	cout<<price<<endl;
+	return 0;
+}
