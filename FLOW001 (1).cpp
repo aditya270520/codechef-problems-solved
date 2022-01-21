915fa2cf3070dd025b9a60e3005e8a95a091d54c
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-21 10:06:50 +0530

    Add files via upload

---
+++ FLOW001 (1).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int TESTCASE = 1;
+	cin>>TESTCASE;
+	while(TESTCASE--){
+	    int A, B, SUM;
+	    cin>>A>>B;
+	    SUM = A+B ;
+	    cout<<SUM<<endl;
+	}
+	    
+	
+
+	return 0;
+}
