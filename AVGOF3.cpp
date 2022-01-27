a9e1455529ddec54d0047b78fbf74d4e307aaea1
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-01-27 09:43:30 +0530

    Add files via upload

---
+++ AVGOF3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int TESTCASE = 1;
+	cin>>TESTCASE;
+	while(TESTCASE--){
+	    int X,A,B;
+	    cin>>X;
+	    A = X-1;
+	    B = X+1;
+	    cout<<A<<" "<<X<<" "<<B<<endl;
+	    
+	}
+	    
+	
+	return 0;
+}
