75e042c251b1528496e5cbbf3665d906636a47a5
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-02-03 09:36:28 +0530

    Add files via upload

---
+++ BADMINTON.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int TESTCASE = 1;
+	cin>>TESTCASE;
+	while(TESTCASE--){
+	    int p, a;
+	    cin>>p;
+	    a = p/2 + 1 ;
+	    cout<< a <<endl;
+	    
+	}
+	return 0;
+}
