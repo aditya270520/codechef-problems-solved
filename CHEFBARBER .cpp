e6586cfb75231fad0db8462b4d728db213fe14d0
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-02-08 14:09:11 +0530

    Add files via upload

---
+++ CHEFBARBER .cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int TESTCASE = 1;
+	cin>>TESTCASE;
+	while(TESTCASE--){
+	    int n,m,a;
+	    cin>>n>>m;
+	    a=n*m;
+	    cout<<a<<endl;
+	}
+	return 0;
+}
