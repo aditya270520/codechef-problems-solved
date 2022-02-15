3835d5d49bb3e5ba2dccb0b1113340d1f7dc7f94
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-02-15 08:17:19 +0530

    Add files via upload

---
+++ HELIUM3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    //your code goes here
+    while(t--){
+        int a,b,x,y;
+        cin>>a>>b>>x>>y;
+        int c=a*b;
+        int z=x*y;
+        (z>=c)?cout<<"yes":cout<<"no";
+        cout<<("\n");
+        
+    }
+	
+	return 0;
+}
