4b5866b9207385f0c10af4513b3cb7e03d3b4509
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-02-17 13:06:03 +0530

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//your code goes here
+int sol(){
+    int n;
+    cin>>n;
+    int p=1000*n;
+    cout<<floor(p/100)<<endl;
+
+    return 0;
+}
+
+
+int main(){
+    int testcase;
+    cin>>testcase;
+    while(testcase--){
+        sol();
+    }
+}
