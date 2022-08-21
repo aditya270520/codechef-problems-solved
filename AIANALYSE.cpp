cf10641bf9ee344a841604a16fb7fcfa497fe0f8
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-08-21 20:02:41 +0530

    Add files via upload

---
+++ AIANALYSE.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    if(n<=1000){
+        cout << "YES"<<"\n";
+    }
+    else{
+        cout << "NO"<<"\n";
+    }
+    
+	return 0;
+}
---
+++ MAXIMUMSUBS.py
@@ -0,0 +1,5 @@
+# cook your dish here
+T = int(input())
+for i in range(T):
+    x = int(input())
+    print(x*2)
