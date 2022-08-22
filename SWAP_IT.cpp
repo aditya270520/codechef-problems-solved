fc7eed6aaac8935720713260360d300702fb67e2
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2022-08-22 14:07:18 +0530

    Add files via upload

---
+++ SWAP_IT.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x, y;
+    cin >> x >> y;
+    cout << y << "\n";
+    cout << x << "\n";
+	return 0;
+}
